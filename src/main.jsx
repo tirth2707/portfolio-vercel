31cc7a5d0cda9da876f98324ce77fb718e4bcf01
Tirth Shah <tirthshah485@gmail.com>
2026-01-23 17:10:03 +0530

    Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App.jsx';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
       <BrowserRouter>
+       <Analytics />
         <App />
       </BrowserRouter>
     </ThemeProvider>
